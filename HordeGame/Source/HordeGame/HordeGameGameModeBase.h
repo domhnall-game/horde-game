1bef8cc32756419b1c8ffa7235d9aca3ffbe62b0
domhnall_game <domhnall.game@gmail.com>
2019-02-26 17:13:41 -0500

    Initial commit

---
+++ HordeGame/Source/HordeGame/HordeGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HordeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORDEGAME_API AHordeGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
