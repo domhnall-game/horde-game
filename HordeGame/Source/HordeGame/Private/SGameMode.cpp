f7c49bca87e74b77aa24af20a1c499bb88c4900a
domhnall_game <domhnall.game@gmail.com>
2019-04-23 09:24:51 -0400

    SGameMode class is now responsible for setting up waves
    SGameMode:
    Added variables to hold: number of bots per spawn, wave counter, and how long between waves
    On StartPlay, calls PrepareForNextWave
    PrepareForNextWave starts a timer that will eventually call StartWave
    StartWave sets the current wave counter and the number of bots to spawn in that wave, and also sets a timer that calls SpawnBotTimerElapsed
    SpawnBotTimerElapsed checks to make sure the correct number of bots have been spawned; when that number is reached, it calls EndWave
    EndWave clears the bot spawn timer and calls PrepareForNextWave

--- HordeGame/Source/HordeGame/Private/SGameMode.cpp
+++ HordeGame/Source/HordeGame/Private/SGameMode.cpp
@@ -2,4 +2,46 @@
 
 #include "SGameMode.h"
 
+#include "TimerManager.h"
 
+ASGameMode::ASGameMode()
+{
+	WaveCount = 0;
+	TimeBetweenWaves = 2.f;
+}
+
+void ASGameMode::StartWave()
+{
+	WaveCount++;
+	NumberOfBotsToSpawn = 2 * WaveCount;
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimerElapsed, 1.f, true, 0.f);
+}
+
+void ASGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	NumberOfBotsToSpawn--;
+
+	if (NumberOfBotsToSpawn <= 0) {
+		EndWave();
+	}
+}
+
+void ASGameMode::StartPlay()
+{
+	Super::StartPlay();
+	PrepareForNextWave();
+}
+
+void ASGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+	PrepareForNextWave();
+}
+
+void ASGameMode::PrepareForNextWave()
+{
+	FTimerHandle TimerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+}
--- HordeGame/Source/HordeGame/Public/SGameMode.h
+++ HordeGame/Source/HordeGame/Public/SGameMode.h
@@ -13,5 +13,32 @@ UCLASS()
 class HORDEGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+protected:
+	int32 WaveCount;
+	//Bots to spawn in single wave
+	int32 NumberOfBotsToSpawn;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+	FTimerHandle TimerHandle_BotSpawner;
 		
+protected:
+	//Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	//Start spawning bots
+	void StartWave();
+	//Stop spawning bots
+	void EndWave();
+	//Set timer for next wave start
+	void PrepareForNextWave();
+
+public:
+	ASGameMode();
+	virtual void StartPlay() override;
 };
